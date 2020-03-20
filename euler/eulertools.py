de5c92c4410cc0865b7a93b72b32b45c7be7f709
dmannix <null_pointer@tuta.io>
2020-03-20 17:35:57 +1100

    began work on project euler problems

---
+++ euler/eulertools.py
@@ -0,0 +1,50 @@
+#module with helper functions for project euler problems
+
+import math
+
+# simple primality test - TODO: more efficient one 
+def is_prime(n):
+
+    for i in range(2,int(math.sqrt(n))):
+
+        if n % i == 0:
+
+            return False
+
+    return True
+
+# generate primes up to n
+def primes_to(n):
+
+    # uses one version of the sieve of eratosthenes
+
+    record = [True for i in range(n+1)]	
+
+    for i in range(2,int(math.sqrt(n))):
+
+        if record[i]:
+
+            j = i*i
+
+            while j <= n:
+
+                record[j] = False
+                j += i
+
+    ret = []
+
+    for k in enumerate(record):
+
+        print(k)
+
+    for i in range(2,len(record)):
+
+        if record[i]:
+
+            ret.append(i)
+
+    return ret
+
+#return the nth prime (a stored list, then a generator?)
+
+# TODO: decompose into list of prime factors
